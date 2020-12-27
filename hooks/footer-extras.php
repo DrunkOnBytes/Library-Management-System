23cf6a1dfcd1c800c9919e2e1b97a7dc3dc64784
RyuuKenshi <ryuukenshi@users.noreply.github.com>
2020-12-27 15:15:09 +0530

    updated footer

--- hooks/footer-extras.php
+++ hooks/footer-extras.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.1.0
     </div>
-    <strong>Developed By Arjun & Diksha</a></strong>
+    <strong>Developed By Arjun, Fahad & Adhiraj</a></strong>
   </footer>
